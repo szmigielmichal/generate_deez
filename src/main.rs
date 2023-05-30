fca208f79160de6459ba03209a819da90f0b2189
michalszmigiel <michal.szmigiel@tsh.io>
2023-05-31 00:31:04 +0200

    set up repo download

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::{process, io};
+use std::{process, io, env, path::Path};
+use git2::{Repository, RemoteCallbacks, Cred};
+use dotenv::dotenv;
 
 fn main() {
+    dotenv().ok();
+
     loop {
         let mut option = String::new();
         println!("Choose an option: 1 - download repo, 2 - add user, 3 - set up new project repo, Q - quit");
         io::stdin().read_line(&mut option).unwrap();
 
         match option.to_string().trim() {
-            "1" => println!("downloading repo"),
+            "1" => download_repo(),
             "2" => println!("adding user"),
             "3" => println!("setting up new repo"),
+            "4" => branch(),
             "q"|"Q" => process::exit(0x0100),
             opt => { println!("Invalid option: {}", opt); continue; }
         }
@@ -25,7 +30,53 @@ fn add_repo() {
 }
 
 fn download_repo() {
+    let mut callbacks = RemoteCallbacks::new();
+      callbacks.credentials(|_url, username_from_url, _allowed_types| {
+        Cred::ssh_key(
+          username_from_url.unwrap(),
+          None,
+          Path::new(&format!("{}/.ssh/id_rsa", env::var("HOME").unwrap())),
+          Some(env::var("PASSPHRASE").unwrap().as_str()),
+        )
+      });
 
+      // Prepare fetch options.
+      let mut fo = git2::FetchOptions::new();
+      fo.remote_callbacks(callbacks);
+
+      // Prepare builder.
+      let mut builder = git2::build::RepoBuilder::new();
+      builder.fetch_options(fo);
+
+      // Clone the project.
+      builder.clone(
+        env::var("GITLAB_URL").unwrap().as_str(),
+        Path::new(&format!("{}/Code/infrastructure-as-code", env::var("HOME").unwrap()))
+      ).unwrap();
+    
+    println!("Finished cloning");
+}
+
+fn branch() {
+    let branch_name = "new-branch";
+    let repo = Repository::open(
+        Path::new(&format!("{}/Code/infrastructure-as-code", env::var("HOME").unwrap()))
+    ).unwrap();
+
+    let object = repo.revparse_single("master").unwrap();
+    let commit = object.as_commit().unwrap();
+
+    repo.branch(&branch_name, &commit, true).unwrap();
+
+    let obj = repo.revparse_single(&("refs/heads/".to_owned() + 
+        branch_name)).unwrap();
+
+    repo.checkout_tree(
+        &obj,
+        None
+    ).unwrap();
+
+    repo.set_head(&("refs/heads/".to_owned() + branch_name)).unwrap();
 }
 
 
